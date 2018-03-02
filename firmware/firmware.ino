7523d13945dd1df05ef9274e663d4fe4f0f1369f
5shekel <yair99@gmail.com>
2018-03-02 19:06:42 +0100

    switch to record

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -18,7 +18,7 @@
 
   attiny85 pinout - https://camo.githubusercontent.com/081b569122da2244ff7de8bae15eb56947d05cc8/687474703a2f2f6472617a7a792e636f6d2f652f696d672f50696e6f7574543835612e6a7067
  *******************************************************************/
-#include "funcs.h"
+
 #include <EEPROM.h>
 
 #define SERVO1PIN 0   // Servo control line (orange) on 0/PWM
@@ -40,7 +40,14 @@ unsigned long  prevValue;
 long lastPressTime = 0;
 
 int pos_middle = 82;
+int trimVal = 0; //soft calbrate cneter
 
+int PROG = 1;
+int outPos;
+
+int servoPos;
+  int potValue;
+  
 void setup() {
 
   irrecv.enableIRIn();
@@ -50,6 +57,9 @@ void setup() {
 
   EEPROM.get(0, pos_middle );
   softSerial.println(pos_middle);
+
+  //EEPROM.get(1, trimVal );
+  softSerial.println(trimVal);
 
   softServo.attach(SERVO1PIN);
   softServo.setMaximumPulse(2200);
@@ -61,17 +71,32 @@ void loop()  {
   if (irrecv.decode(&results)) {
     switch (results.value) {
       case 0xFFA25D:
-        softSerial.print("[CH-] pos_mid "); softSerial.println(pos_middle);
-        pos_middle--;
+        softSerial.print("[CH-] pos_mid "); softSerial.println(trimVal);
+        trimVal--;
+
+        softSerial.print(servoPos); softSerial.print(" ");
+        softSerial.print(trimVal); softSerial.print(" ");
+        softSerial.print(outPos); softSerial.print(" ");
+        softSerial.print(potValue); softSerial.print(" ");
+        softSerial.println();
+
         break;
       case 0xFFE21D:
-        softSerial.print("[CH+] posmid  "); softSerial.println(pos_middle);
-        pos_middle++;
+        softSerial.print("[CH+] posmid  "); softSerial.println(trimVal);
+        trimVal++;
+
+        softSerial.print(servoPos); softSerial.print(" ");
+        softSerial.print(trimVal); softSerial.print(" ");
+        softSerial.print(outPos); softSerial.print(" ");
+        softSerial.print(potValue); softSerial.print(" ");
+        softSerial.println();
+
         break;
       case 0xFF629D:
-        EEPROM.put(0, pos_middle );
-        softSerial.print("[CH] write "); softSerial.println(pos_middle);
+        EEPROM.put(0, trimVal );
+        softSerial.print("[CH] write "); softSerial.println(trimVal);
         break;
+
       case 0xFF906F:
         softSerial.print("value dump\n");
         softSerial.print("pos_middle  -  "); softSerial.println(pos_middle);
@@ -87,32 +112,55 @@ void loop()  {
 
   }
 
-  int servoPos;
-  int potValue = analogRead(POTPIN);              // Read voltage on potentiometer
+  servoPos;
+  potValue = analogRead(POTPIN);              // Read voltage on potentiometer
 
   // map the range of pot to limited map range on the servo.
   // a narrow band of the full range.
   // in both directions, with a stop space in middle
-  tongueDiff(potValue);
-/*
-  if ( potValue < 350 ) {
-    servoPos = map(potValue, 0, 350, 20, 80); //the88p0 has less
-  } else if (potValue > 650) {
-    servoPos = map(potValue, 1024, 650, 150, 90);
-  } else {
-    servoPos = pos_middle ;// 82; //86 with the 880
-  }
-*/
 
-  //to calibrate override the above
-  //servoPos = map(potValue, 0, 1024, 0, 179);
-  if (servoPos != prevServoPos) {
-    softSerial.print(servoPos);
-    softSerial.print(" ");
-    softSerial.println(potValue);
-    softServo.write(servoPos);
+  //tongueDiff(potValue);
+  /*
+    topSpeedL = 20;
+    topSpeedR = 150;
+    lowSpeedL = 80;
+    lowSpeedR = 90;
+  */
+
+  PROG = 2;
+  switch (PROG) {
+    case 1:
+      if ( potValue < 350 ) {
+        servoPos = map(potValue, 0, 350, 20, 80); //the88p0 has less
+      } else if (potValue > 650) {
+        servoPos = map(potValue, 650, 1024, 90, 150);
+      } else {
+        servoPos = pos_middle ;// 82; //86 with the 880
+      }
+
+      if (servoPos != prevServoPos) {
+        softSerial.print(servoPos); softSerial.print(" ");
+        softSerial.print(potValue); softSerial.print(" ");
+        softSerial.println();
+      }
+      prevServoPos = servoPos;
+
+      break;
+    case 2:
+      //to calibrate override the above
+      servoPos = potValue / 4 ; //map(servoPos, 0, 1024, 0, 179);
+      outPos = trimVal + servoPos;
+
+      //if (outPos != prevServoPos) {
+
+      //}
+      prevServoPos = outPos;
+      break;
   }
-  prevServoPos = servoPos;
+
+  softServo.write(outPos);
+
+  //analogWrite(SERVO1PIN, servoPos); //doesnt work, servo need PPM 0.5-2ms
 
   SoftwareServo::refresh();
   delay(15);                              // waits 15ms for the servo to reach the position
@@ -122,3 +170,14 @@ void loop()  {
 
 
 
+byte tongueDiff(int val, byte mid, int  ) {
+  byte servoPos;
+  if ( val < 350 ) {
+    servoPos = map(val, 0, 350, 20, 80); //the88p0 has less
+  } else if (val > 650) {
+    servoPos = map(val, 1024, 650, 150, 90);
+  } else {
+    servoPos = pos_middle ;// 82; //86 with the 880
+  }
+  return servoPos;
+}
--- firmware/funcs.h
+++ firmware/funcs.h
@@ -1,12 +1,1 @@
 
-byte tongueDiff(int val) {
-    byte servoPos;
-    if ( val < 350 ) {
-      servoPos = map(val, 0, 350, 20, 80); //the88p0 has less
-    } else if (val > 650) {
-      servoPos = map(val, 1024, 650, 150, 90);
-    } else {
-      servoPos = pos_middle ;// 82; //86 with the 880
-    }
-    return servoPos;
-  }
